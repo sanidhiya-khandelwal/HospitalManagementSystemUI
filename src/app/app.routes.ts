2cd103f0cf66cc6836c683c4187bcd99d8c03a89
sanidhiya-khandelwal <sanidhiyakhandelwal450@gmail.com>
2025-02-22 19:06:38 +0530

    feat: doctor components created with routing and router outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DoctorListComponent } from './components/doctors/doctor-list/doctor-list.component';
+import { DoctorDetailComponent } from './components/doctors/doctor-detail/doctor-detail.component';
+import { DoctorFormComponent } from './components/doctors/doctor-form/doctor-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'doctors', component: DoctorListComponent },
+    { path: 'doctors/:id', component: DoctorDetailComponent },
+    { path: 'create-doctor', component: DoctorFormComponent },
+    { path: '', redirectTo: 'doctors', pathMatch: 'full' },
+    // { path: '**', component: NoAvailablePageComponent },
+];
---
+++ src/app/components/doctors/doctor-form/doctor-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-form',
+  imports: [],
+  templateUrl: './doctor-form.component.html',
+  styleUrl: './doctor-form.component.css'
+})
+export class DoctorFormComponent {
+
+}
